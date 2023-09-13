b934459d264766d89116e06a254d5f4217c68f87
Costin Ionuț <51002637+staark-dev@users.noreply.github.com>
2023-09-13 21:30:11 +0200

    gh-pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Staark Development &heart;
+          Welcome to Staark Development ❤️
+          <h1>You now browse to best site of resources.</h1>
         </p>
         <a
           className="App-link"
