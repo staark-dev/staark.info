5a34ff501556f9f66714ee122e62be3705987126
Costin Ionuț <51002637+staark-dev@users.noreply.github.com>
2023-09-13 21:42:03 +0200

    gh-pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Staark Development ❤️
-          <h1>You now browse to best site of resources.</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+	<div className="App">
+		<header className="App-header">
+			<p>
+				Welcome to Staark Development ❤️
+				<h1>You now browse to best site of resources.</h1>
+			</p>
+		</header>
+	</div>
+	);
 }
-
-export default App;
