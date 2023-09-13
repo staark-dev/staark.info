f84090ff39ac6c22ad948164f3c325361782349f
Costin Ionuț <51002637+staark-dev@users.noreply.github.com>
2023-09-13 21:03:45 +0200

    Fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Staark Development &heart;
         </p>
         <a
           className="App-link"
