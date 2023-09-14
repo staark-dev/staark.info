413c445e82214b9775c4b9e149d5c4524a74dba2
Costin Ionuț <51002637+staark-dev@users.noreply.github.com>
2023-09-14 18:47:35 +0200

    gh-pages

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,12 +4,12 @@ export default function Navbar() {
     const user = false;
 
     return (
-        <>
+        <ul className="nav-links">
             <Link to='/' className='nav-link'>Home</Link>
             <Link to='/services' className='nav-link'>Services</Link>
             <Link to='/projects' className='nav-link'>Projects</Link>
             <Link to='/contact' className='nav-link'>Contact</Link>
             <Outlet />
-        </>
+        </ul>
     )
 }
