c6251db7c8292d86d9ba97a7a3525f5cfd041603
Costin Ionuț <51002637+staark-dev@users.noreply.github.com>
2023-09-16 00:02:08 +0200

    gh-pages

--- src/layouts/Footer/index.jsx
+++ src/layouts/Footer/index.jsx
@@ -3,16 +3,16 @@ import { Link, Outlet } from "react-router-dom";
 
 export default function Footer() {
     return (
-        <footer className="footer mt-auto">
+        <footer className="footer mt-auto py-2">
             <div className="my-2 container">
-                <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                    <li class="nav-item"><Link to="/" className="nav-link px-2 text-body-secondary">Home</Link></li>
-                    <li class="nav-item"><Link to="/features" className="nav-link px-2 text-body-secondary">Features</Link></li>
-                    <li class="nav-item"><Link to="/faq" className="nav-link px-2 text-body-secondary">FAQs</Link></li>
-                    <li class="nav-item"><Link to="/privacy" className="nav-link px-2 text-body-secondary">Privacy</Link></li>
-                    <li class="nav-item"><Link to="/terms" className="nav-link px-2 text-body-secondary">Terms & Conditions</Link></li>
+                <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li className="nav-item"><Link to="/" className="nav-link px-2 text-body-secondary">Home</Link></li>
+                    <li className="nav-item"><Link to="/features" className="nav-link px-2 text-body-secondary">Features</Link></li>
+                    <li className="nav-item"><Link to="/faq" className="nav-link px-2 text-body-secondary">FAQs</Link></li>
+                    <li className="nav-item"><Link to="/privacy" className="nav-link px-2 text-body-secondary">Privacy</Link></li>
+                    <li className="nav-item"><Link to="/terms" className="nav-link px-2 text-body-secondary">Terms & Conditions</Link></li>
                 </ul>
-                <p class="text-right">© 2023 Staark Development ❤️<br /> &copy; All right reserved.</p>
+                <p className="text-right">© 2023 Staark Development ❤️<br /> &copy; All right reserved.</p>
             </div>
             <Outlet />
         </footer>
