9d220fe9d05834707bfc0e949f03de7998653c5c
Costin Ionuț <51002637+staark-dev@users.noreply.github.com>
2023-09-17 10:58:25 +0200

    email.

--- src/hooks/sendMail.jsx
+++ src/hooks/sendMail.jsx
@@ -1,48 +1,28 @@
-import React, { Fragment, useState } from "react";
+import React, { useState, useRef, Fragment } from "react";
 import { Alert } from "react-bootstrap";
+import emailjs from '@emailjs/browser';
 
-export default function FormMail() {
-    const [email, setEmail] = useState("");
-    const [name, setName] = useState("");
-    const [subject, setSubject] = useState("");
-    const [message, setMessage] = useState("");
-
+export const FormSendEmails = () => {
+    const form = useRef();
     const [errorMessages, setErrorMessages] = useState([]);
     const [showErrors, setShowErrors] = useState(false);
 
     let errors = [];
 
-    /*const ValidateEmail = (email) => {
-        if(new RegExp(/\S+@\S+\.\S+/).test(email)) {
-            return true;
-        }
-
-        return false;
-    }*/
-
-    const formValidation = () => {
-        setErrorMessages([]);
-
-        const isNameValid = (name !== "");
-        const isMessageValid = ( message !== "");
-        const isSubjectValid = (subject !== "");
-
-        if(!isNameValid) errors.push("Name is not valid, please try again.");
-        if(!email) errors.push("Email field is empty, please try again.");
-        if(!isMessageValid) errors.push("Message is not valid, please try again.");
-        if (!isSubjectValid) errors.push("Subject is not valid, please try again.");
-        if (errors.length > 0) {
-            setShowErrors({ showErrors: true });
-            setErrorMessages(errors);
-        } 
-        else {
-            setShowErrors({ showErrors: false });
-            alert("Email Sent");
-        }
-    }
-
-    const handleSubmit = (e) => {
+    // Send emails using library EmailJS
+    const sendEmail = (e) => {
         e.preventDefault();
+        sendFormValidation(e);
+    };
+
+    const validStr = (str) => str ? true : false;
+
+    let validateEmail = (email) => {
+        return email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    };
+
+    const sendFormValidation = (e) => {
+        setErrorMessages([]);
 
         const inputs = e.target.elements;
         const data = {};
@@ -51,22 +31,33 @@ export default function FormMail() {
             if (inputs[i].name) {
                 data[inputs[i].name] = inputs[i].value;
             }
+
+            let label = inputs[i].id.replace("contact", "", inputs[i].id);
+
+            if( !validStr(inputs[i].value) ) errors.push(label + " is not valid, please try again.");
         }
 
-        { showErrors ? errorMessages.map((item) => {
-                console.log("");
-                console.log(item);
-            }) : console.log("Submiting form../r/n" + data);
+        if (errors.length > 0) {
+            setShowErrors({ showErrors: true });
+            setErrorMessages(errors);
+        } 
+        else {
+            setShowErrors({ showErrors: false });
+            emailjs.sendForm('service_eisb8mm', 'template_7c27wdi', form.current, '40mv4rjxYp44UQTWs')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
         }
-    }
-
+    };
 
     return (
         <Fragment>
             <div className="row justify-content-center">
                 <div className="col-12 col-md-12 col-lg-10">
                     <h1 class="mb-3">Contact Us</h1>
-                    <form onSubmit={handleSubmit}>
+                    <form ref={form} onSubmit={sendEmail}>
                         { showErrors ? errorMessages.map((item) => { 
                             return (<Alert variant="danger">{item}</Alert>); 
                         }) : null }
@@ -75,34 +66,34 @@ export default function FormMail() {
                             <div class="col-12 col-md-6">
                                 <div class="form-group mb-5">
                                     <label class="form-label" for="contactName">Full Name</label>
-                                    <input className="form-control" label="Name" id="contactName" type ="text" onChange = {e => setName({name: e.target.value})} />
+                                    <input className="form-control" label="Name" name="user_name" id="contactName" type ="text" />
                                 </div>
                             </div>
 
                             <div class="col-12 col-md-6">
                                 <div class="form-group mb-5">
                                     <label class="form-label" for="contactEmail">Email</label>
-                                    <input className="form-control" label="Email" id="contactEmail" type ="email" onChange ={e => setEmail({email: e.target.value})} />
+                                    <input className="form-control" name="user_email" label="Email" id="contactEmail" type ="email" />
                                 </div>
                             </div>
 
                             <div class="col-12 col-md-12">
                                 <div class="form-group mb-5">
                                     <label class="form-label" for="contactSubject">Subject</label>
-                                    <input className="form-control" label="Subject" id="contactSubject" type ="text" onChange ={e => setSubject({subject: e.target.value})} />
+                                    <input className="form-control" label="Subject" id="contactSubject" type ="text" />
                                 </div>
                             </div>
 
                             <div class="col-12 col-md-12">
                                 <div class="form-group mb-5">
                                     <label class="form-label" for="contactMessage">Message</label>
-                                    <textarea className="form-control" rows="4" id="contactMessage" label="Message" onChange ={e => setMessage({message: e.target.value})} />
+                                    <textarea className="form-control" name="message" rows="4" id="contactMessage" label="Message" />
                                 </div>
                             </div>
                         </div>
                         <div className="row">
                         <div class="d-grid gap-2 col-6 mx-auto">
-                            <button class="btn btn-primary btn-sm btn btn-success" onClick={formValidation} type="submit">Send me Mail!</button>
+                            <button class="btn btn-primary btn-sm btn btn-success" type="submit">Send me Mail!</button>
                         </div>
                         </div>
                     </form>
@@ -111,3 +102,5 @@ export default function FormMail() {
         </Fragment>
     )
 }
+
+export default FormSendEmails;
--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -4,17 +4,17 @@ import Header from "../../layouts/Header";
 import Layout from "../../layouts/Main";
 import Footer from "../../layouts/Footer";
 import logo from "../contact/images/mail.png";
-import FormMail from "../../hooks/sendMail";
+import FormSendEmails from "../../hooks/sendMail";
 
 export default function Contact() {
     //const sendMail = useState(false);
 
     return (<Fragment>
         <Header />
-        <Layout style={{heigh: "100vh"}}>
+        <Layout>
             <img src={logo} alt="" className="background" />
             <div class="container my-5" style={{width: "960px", float: "right" }}>
-                <FormMail />
+                <FormSendEmails />
             </div>
         </Layout>
         <Footer />
